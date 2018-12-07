cf61ccda00e4c9e7b10649c63dc2697cd92a808a
dowolken <45375823+dowolken@users.noreply.github.com>
2018-12-07 13:55:23 +0800

    Update 20181206Cloud-Homework.java

--- 20181206Cloud-Homework.java
+++ 20181206Cloud-Homework.java
@@ -17,7 +17,7 @@ public class Word{
         for(int i=0;i<word.length()-1; i++)
         {if(((word.substring(i,i+1)).equals("A"))&&(!(word.substring(i+1,i+2)).equals("A")))
             {word=word.substring(0,i)+word.substring(i+1,i+2)+word.substring(i,i+1)+word.substring(i+2);
-            i++}
+            i++;}
         }
         return word;
         
